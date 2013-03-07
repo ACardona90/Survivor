03cc2f90ff39adda760e4e9764743614c3a38940
Alberto Cardona <ACardona90@gmail.com>
2013-03-07 12:35:17 +0100

    creacion de fondo y bola(la bola aun no funciona)

--- pong.py
+++ pong.py
@@ -7,26 +7,51 @@ import sys, pygame
 from pygame.locals import *	
  
 # Constantes
-width = 740
-height = 580 
+width = 1024
+height = 768 
 
 # Clases
 # ---------------------------------------------------------------------
  
+class Bola(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image("imagenes/bola.jpg", True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.centery = HEIGHT / 2
+        self.speed = [0.5, -0.5]
+
 # ---------------------------------------------------------------------
  
 # Funciones
 # ---------------------------------------------------------------------
- 
+def load_image(filename, transparent=False):
+        try: image = pygame.image.load(filename)
+        except pygame.error, message:
+                raise SystemExit, message
+        image = image.convert()
+        if transparent:
+                color = image.get_at((0,0))
+                image.set_colorkey(color, RLEACCEL)
+        return image
+
 # ---------------------------------------------------------------------
  
 def main():
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Survivor pong")
+
+    background_image = load_image('imagenes/fondo.jpg')
+    
     while True:
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
+                
+        screen.blit(background_image, (0,0))
+        screen.blit(bola.image, bola.rect)
+        pygame.display.flip()
     return 0
  
 if __name__ == '__main__':
